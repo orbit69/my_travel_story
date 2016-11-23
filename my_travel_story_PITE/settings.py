c40c1dd2b23e7c5dae136cc00fc8689893f2c04f
Szymon <szymon.slonina@gmail.com>
2016-11-24 00:50:22 +0100

    updated to latest master

--- my_travel_story_PITE/settings.py
+++ my_travel_story_PITE/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'my_travel_story_PITE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pite',
+        'PASSWORD': 'Sz011235813S',
+        'USER': 'postgres',
+        'PORT': '5432',
+        'HOST': 'localhost',
     }
 }
 
