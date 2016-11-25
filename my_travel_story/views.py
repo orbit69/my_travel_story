fde34e4fd40344f3a6a2f4c5cf7fdeb2c48ed5eb
Szymon <szymon.slonina@gmail.com>
2016-11-25 18:08:21 +0100

    refactor for pull req

--- my_travel_story/views.py
+++ my_travel_story/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import *
@@ -8,10 +9,7 @@ from .models import *
 from my_travel_story.forms import UserForm, UserProfileForm
 
 def index(request):
-    try:
-        user_login = request.session['login']
-    except KeyError, e:
-        return HttpResponseRedirect(reverse('login'))
+    user_login = request.session['login']
 
     picture_path = UserProfile.objects.get(id=User.objects.get(username=user_login).id).picture.name.split('/')[-1]
     picture_path = picture_path.encode('ascii', 'ignore')
@@ -24,7 +22,6 @@ def index(request):
     return render(request, 'index.html', queries)
 
 def register(request):
-
     registered = False
 
     if request.method == 'POST':
@@ -64,7 +61,6 @@ def register(request):
 
 
 def user_login(request):
-
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -85,8 +81,7 @@ def user_login(request):
     else:
         return render(request, 'login.html', {})
 
-
-def logout(request):
+def user_logout(request):
     del request.session['login']
-
+    logout(request)
     return HttpResponseRedirect(reverse('login'))
