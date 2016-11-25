98259e111b030a38121212dd22d9b3f2b3b25e72
mMarzeta <94maciekm@gmail.com>
2016-11-25 20:17:26 +0100

    Logout button on /index

--- my_travel_story/forms.py
+++ my_travel_story/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
 
 
 class UserProfileForm(forms.ModelForm):
