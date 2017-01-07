2b7e71d5b48d8099a6a9f03306c5e26430e324b5
Szymon <szymon.slonina@gmail.com>
2017-01-07 16:56:19 +0100

    added sharek_link.

--- my_travel_story/static/scripts/index_script.js
+++ my_travel_story/static/scripts/index_script.js
@@ -234,7 +234,6 @@ function sendPlaceData(){
     childElems = this.getElementsByTagName("p");
     var name = childElems[0].firstChild.nodeValue;
     var date = childElems[1].firstChild.nodeValue;
-
     var csrftoken = getCookie('csrftoken');
     var req = new XMLHttpRequest();
     req.open("POST","",true);
@@ -250,10 +249,9 @@ function sendPlaceData(){
 
 function giveChildFunc(){
 
-    sideBar = document.getElementsByClassName("side_bar_nav");
-    sideBar = sideBar[0];
-    sideBarChildren = sideBar.getElementsByTagName("div");
+    sideBar = document.getElementById("side-bar");
 
+    sideBarChildren = sideBar.getElementsByTagName("div");
 
     for(var ind=0;ind<sideBar.childElementCount;ind++){
         sideBarChildren[ind].addEventListener("click", sendPlaceData);
