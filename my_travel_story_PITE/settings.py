8de8e42dee62d8ab5fb9a4243e154726c99d3c36
Szymon <szymon.slonina@gmail.com>
2017-01-08 18:48:10 +0100

    'a'

--- my_travel_story_PITE/settings.py
+++ my_travel_story_PITE/settings.py
@@ -84,8 +84,7 @@ WSGI_APPLICATION = 'my_travel_story_PITE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql'
     }
 }
 DATABASES['default'] = dj_database_url.config()
